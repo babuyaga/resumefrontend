803a27bae3ce9360e88a35ca6df2dd5933f87d7a
babuyaga <77667412+babuyaga@users.noreply.github.com>
2023-04-23 00:13:58 +0530

    dashboard changes WIP

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -12,6 +12,17 @@ function Dashboard() {
 const [checked,setchecked] = useState(false);
 const {handleSignout,loginWithGoogle} = useContext(authContext);
 
+const documentComp=()=>{
+return (<div className="item-documents-section--dashboard">
+                                        <div className="image-item-documents--dashboard"></div>
+                                        <div className="title-item-documents--dashboard"><span>My Resume</span></div>
+                                        <div className="subtitle-item-documents--dashboard"><span>Last updated at 2:30 PM today</span></div>
+                                        <div className="tag-holder-item-documents--dashboard">
+                                                <div className="tags-document--dashboard"></div>
+                                                <div className="tags-document--dashboard"></div>
+                                        </div>
+                                    </div>);
+}
 
 return (  <div className="dashboard-page">
                 <div className="navbar-general--dashboard">
@@ -32,6 +43,7 @@ return (  <div className="dashboard-page">
                                     <div className="profile-block--menu"></div>
                                  </div>
                             </div> 
+                            
                         </div>
                         
                         <div className="content-holder--dashboard component-content--dashboard">
@@ -39,26 +51,30 @@ return (  <div className="dashboard-page">
                                     <div className="component-banner-section--dashboard banner-text--dashboard"><span>Get access to SOP Writer and other premium features at just 570 / month</span></div>
                             </div>
                             <div className="section-content-holder--dashboard main-buttons-section--dashboard">
-                                <div className="component-buttons-sections--dashboard buttons-section-title--dashboard"><span>Hello Jerry</span><span>Let's get you started</span></div>
+                                <div className="component-buttons-sections--dashboard buttons-section-title--dashboard"><span className="username-buttons-section--dashboard">Hello Jerry!</span><span className="subtitle-buttons-section--dashboard">Let's get you started</span></div>
                                 <div className="component-buttons-sections--dashboard"> <div className="feature-button--dashboard"><span>Resume</span></div> </div>
                                 <div className="component-buttons-sections--dashboard"> <div className="feature-button--dashboard"><span>SOP</span></div> </div>
                                 <div className="component-buttons-sections--dashboard"> <div className="feature-button--dashboard"><span>Cover Letter</span></div> </div>
                             </div>
+                        
+                            <div className="section-content-holder--dashboard horizontal-rule--holder">
+                                <div className="horizontal-rule"><hr></hr></div>
+                            </div>
                             <div className="section-content-holder--dashboard documents-section--dashboard">
-                                    <div className="component-documents-section--dashboard documents-section-title--dashboard"><span>Documents</span><span>View all</span></div>
+                                    <div className="component-documents-section--dashboard documents-section-title--dashboard"><span className="title-documents--section">Your Documents</span><span className="viewall-documents--section">View all</span></div>
                                     <div className="component-documents-section--dashboard documents-buttons--dashboard">
-                                         <div className="buttons">Buttons</div>
-                                         <div className="buttons">Buttons</div>
-                                         <div className="buttons">Buttons</div>
-                                         <div className="buttons">Buttons</div>
+                                         <div className="buttons-documents-component" id="selected-button-document"><span>All</span></div>
+                                         <div className="buttons-documents-component"><span>Resumes</span></div>
+                                         <div className="buttons-documents-component"><span>SOPs</span></div>
+                                         <div className="buttons-documents-component"><span>Cover Letters</span></div>
 
                                     </div>
-                                    <div className="component-documents-section--dashboard">
-                                    <div className="item-documents-section--dashboard">Document 1</div>
-                                    <div className="item-documents-section--dashboard">Document 1</div>
-                                    <div className="item-documents-section--dashboard">Document 1</div>
-                                    <div className="item-documents-section--dashboard">Document 1</div>    
-                                    </div>
+                                    <div className="component-documents-section--dashboard documents-display-section--dashboard">
+                                    {documentComp()}
+                                    {documentComp()}
+                                    {documentComp()}
+                                    {documentComp()}
+                                     </div>
                             </div>
                             <div className="section-content-holder--dashboard callback-section--dashboard">
                                         <div className="component-callback-section--dashboard component-desktop">Get Expert Help</div>
